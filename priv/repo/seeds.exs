1d7d4a673996a701f437b8b22b6d95d9bc44b9a0
swizkon <jonas@jerndin.se>
2017-09-11 20:59:21 +0200

    New completion goal.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -87,6 +87,7 @@ DatabaseSeeder.completed_leg(1,14) |> Repo.insert!
 DatabaseSeeder.completed_leg(1,15) |> Repo.insert!
 DatabaseSeeder.completed_leg(1,16) |> Repo.insert!
 DatabaseSeeder.completed_leg(1,17) |> Repo.insert!
+DatabaseSeeder.completed_leg(1,18) |> Repo.insert!
 
 Repo.insert!(%Traveler{user: jonas, journey: tapout, name: jonas.name <> ": " <> tapout.name})
 
