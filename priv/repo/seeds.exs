c355b7db185dc1c491e225ba5d570e52f5b09d78
swizkon <jonas@jerndin.se>
2017-09-20 21:39:48 +0200

    Completed WOD.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -66,7 +66,16 @@ tapout = Repo.insert!(%Journey{
         %Leg {"ordinal": 18, "name": "WEEK 3: Yoga XT" },
         %Leg {"ordinal": 19, "name": "WEEK 3: Legs & Back" },
         %Leg {"ordinal": 20, "name": "WEEK 3: Sprawl & Brawl" },
-        %Leg {"ordinal": 21, "name": "WEEK 3: Rest Day" }
+        %Leg {"ordinal": 21, "name": "WEEK 3: Rest Day" },
+        
+        %Leg {"ordinal": 22, "name": "WEEK 4: Competition Core" },
+        %Leg {"ordinal": 23, "name": "WEEK 4: Buns & Guns + Ultimate Abs" },
+        %Leg {"ordinal": 24, "name": "WEEK 4: Muay Thai" },
+        %Leg {"ordinal": 25, "name": "WEEK 4: Yoga XT" },
+        %Leg {"ordinal": 26, "name": "WEEK 4: Sprawl & Brawl" },
+        %Leg {"ordinal": 27, "name": "WEEK 4: Cardio XT + Ultimat Abs" },
+        %Leg {"ordinal": 28, "name": "WEEK 4: Rest Day" }
+        
     ]
 })
 
@@ -77,6 +86,7 @@ DatabaseSeeder.completed_leg(1, 4) |> Repo.insert!
 DatabaseSeeder.completed_leg(1, 5) |> Repo.insert!
 DatabaseSeeder.completed_leg(1, 6) |> Repo.insert!
 DatabaseSeeder.completed_leg(1, 7) |> Repo.insert!
+
 DatabaseSeeder.completed_leg(1, 8) |> Repo.insert!
 DatabaseSeeder.completed_leg(1, 9) |> Repo.insert!
 DatabaseSeeder.completed_leg(1,10) |> Repo.insert!
@@ -84,12 +94,18 @@ DatabaseSeeder.completed_leg(1,11) |> Repo.insert!
 DatabaseSeeder.completed_leg(1,12) |> Repo.insert!
 DatabaseSeeder.completed_leg(1,13) |> Repo.insert!
 DatabaseSeeder.completed_leg(1,14) |> Repo.insert!
+
 DatabaseSeeder.completed_leg(1,15) |> Repo.insert!
 DatabaseSeeder.completed_leg(1,16) |> Repo.insert!
 DatabaseSeeder.completed_leg(1,17) |> Repo.insert!
 DatabaseSeeder.completed_leg(1,18) |> Repo.insert!
 DatabaseSeeder.completed_leg(1,19) |> Repo.insert!
 DatabaseSeeder.completed_leg(1,20) |> Repo.insert!
+DatabaseSeeder.completed_leg(1,21) |> Repo.insert!
+
+DatabaseSeeder.completed_leg(1,22) |> Repo.insert!
+DatabaseSeeder.completed_leg(1,23) |> Repo.insert!
+
 
 Repo.insert!(%Traveler{user: jonas, journey: tapout, name: jonas.name <> ": " <> tapout.name})
 
